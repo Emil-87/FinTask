0c7b727185cc86952622b793e44c3d50077ed96a
Emil-87 <emil.pegas@gmail.com>
2023-02-17 00:05:51 +0600

    secondCommit

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -8,3 +8,10 @@
 }
 string[] finalArray = { "Hello", "2", "World", ";-)" };
 string[] emptyArray = new string[4];
+for (int i = 0; i < finalArray.Length; i++)
+{
+    if (finalArray[i].Length <= 3)
+    {
+        emptyArray[i] = finalArray[i];
+    }
+}
