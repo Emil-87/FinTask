6887acd23b65602c0b0831424db081f3c0a1c293
Emil-87 <emil.pegas@gmail.com>
2023-02-17 00:04:20 +0600

    firstCommit

---
+++ FinalTask/Program.cs
@@ -0,0 +1,10 @@
+﻿void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+string[] finalArray = { "Hello", "2", "World", ";-)" };
+string[] emptyArray = new string[4];
